413fae74a6501912be2aa04897aa851ee3ca61a6
Haydar Ali Ismail <haydar_ai@yahoo.com>
2015-04-12 10:30:16 +0700

    init commit

---
+++ Median Filter/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Median_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEdited = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdited)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(350, 350);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxEdited
+            // 
+            this.pictureBoxEdited.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxEdited.Location = new System.Drawing.Point(422, 12);
+            this.pictureBoxEdited.Name = "pictureBoxEdited";
+            this.pictureBoxEdited.Size = new System.Drawing.Size(350, 350);
+            this.pictureBoxEdited.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxEdited.TabIndex = 1;
+            this.pictureBoxEdited.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(287, 368);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Open image";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonFilter
+            // 
+            this.buttonFilter.Location = new System.Drawing.Point(422, 368);
+            this.buttonFilter.Name = "buttonFilter";
+            this.buttonFilter.Size = new System.Drawing.Size(75, 23);
+            this.buttonFilter.TabIndex = 3;
+            this.buttonFilter.Text = "Apply filter";
+            this.buttonFilter.UseVisualStyleBackColor = true;
+            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.buttonFilter);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.pictureBoxEdited);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdited)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxEdited;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonFilter;
+    }
+}
+
---
+++ Median Filter/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Median_Filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Choose an image";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBoxOriginal.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBoxOriginal.Image = new Bitmap(ofd.FileName);
+                }
+            }
+        }
+
+        private void buttonFilter_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxOriginal.Image == null)
+                return;
+            else
+                MedianFilter();
+        }
+
+        private void MedianFilter()
+        {
+            Bitmap original = new Bitmap(pictureBoxOriginal.Image);
+            int width = original.Width;
+            int height = original.Height;
+            Bitmap edited = new Bitmap(width, height);
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    Color[] window = new Color[9];
+                    int[] windowR = new int[9];
+                    int[] windowG = new int[9];
+                    int[] windowB = new int[9];
+                    int count = 0;
+                    for (int i = x-1; i < x+2; i++)
+                    {
+                        for (int j = y-1; j < y+2; j++)
+                        {
+                            window[count] = original.GetPixel(i, j);
+                            windowR[count] = window[count].R;
+                            windowG[count] = window[count].G;
+                            windowB[count] = window[count].B;
+                            count++;
+                        }
+                    }
+                    Array.Sort(windowR);
+                    Array.Sort(windowG);
+                    Array.Sort(windowB);
+                    int r = windowR[9 / 2];
+                    int g = windowG[9 / 2];
+                    int b = windowB[9 / 2];
+                    Color color = Color.FromArgb(r, g, b);
+                    edited.SetPixel(x, y, color);
+                }
+            }
+            pictureBoxEdited.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxEdited.Image = edited;
+        }
+    }
+}
